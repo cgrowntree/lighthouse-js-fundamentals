b85b1c838c1dcb6e7b632c38f2c0675ce7674276
Christopher <cgrowntree@gmail.com>
2018-01-05 20:40:21 +0000

    Last bit of js-fundamentals code

---
+++ mergeArrays.js
@@ -0,0 +1,11 @@
+function merge(array1, array2) {
+  var array3 = array1.concat(array2);
+  for (var i; i < array3.length; i++) {
+    return array3
+  }
+  return array3.sort()
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
